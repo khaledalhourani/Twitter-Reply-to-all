5d0b115ec81d272bc1895e8a0e05800208201f3c
Khaled AlHourani <me@khaledalhourani.com>
2010-09-10 03:07:51 +0300

    added icon as base64 image

--- twitter_reply_to_all.user.js
+++ twitter_reply_to_all.user.js
@@ -7,8 +7,13 @@
 // @include       https://twitter.com*
 // @include       https://www.twitter.com*
 // @require       http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js
-// @version       1.0
+// @version       1.1
 // ==/UserScript==
+
+
+var icon = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAQCAYAAAB3AH1ZAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAyBpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw/eHBhY2tldCBiZWdpbj0i77u/IiBpZD0iVzVNME1wQ2VoaUh6cmVTek5UY3prYzlkIj8+IDx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IkFkb2JlIFhNUCBDb3JlIDUuMC1jMDYwIDYxLjEzNDc3NywgMjAxMC8wMi8xMi0xNzozMjowMCAgICAgICAgIj4gPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4gPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIgeG1sbnM6eG1wPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvIiB4bWxuczp4bXBNTT0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wL21tLyIgeG1sbnM6c3RSZWY9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9zVHlwZS9SZXNvdXJjZVJlZiMiIHhtcDpDcmVhdG9yVG9vbD0iQWRvYmUgUGhvdG9zaG9wIENTNSBXaW5kb3dzIiB4bXBNTTpJbnN0YW5jZUlEPSJ4bXAuaWlkOjAwRUYwODU2QkM1OTExREZCOTY4QThBREU0OUEyRUJDIiB4bXBNTTpEb2N1bWVudElEPSJ4bXAuZGlkOjAwRUYwODU3QkM1OTExREZCOTY4QThBREU0OUEyRUJDIj4gPHhtcE1NOkRlcml2ZWRGcm9tIHN0UmVmOmluc3RhbmNlSUQ9InhtcC5paWQ6MDBFRjA4NTRCQzU5MTFERkI5NjhBOEFERTQ5QTJFQkMiIHN0UmVmOmRvY3VtZW50SUQ9InhtcC5kaWQ6MDBFRjA4NTVCQzU5MTFERkI5NjhBOEFERTQ5QTJFQkMiLz4gPC9yZGY6RGVzY3JpcHRpb24+IDwvcmRmOlJERj4gPC94OnhtcG1ldGE+IDw/eHBhY2tldCBlbmQ9InIiPz7EOVKSAAABC0lEQVR42sSVLwvCQBiHb2OizWqUgSCY/B4WrRo0DQwWDX4Ao2ITLhm0GP0GIlgUm00Y+hHswvmbvCvqdu/dgi88cOz2vPdnP5hQSgkbolIyGtq5b19I5YmPCsMwHq7BHJzjB77vC10FQZDoSym/3ncT+kxBm+ZzwrzY/q8NLMCQxgdwBF3gMBc38l1NszuogSVoWdyE1v+1gT6Y0bgJKmAHBsxFjfykGxiBG6jTKSZ0Em6x/bQQlsEV5EEPrAxDyPJ1IdyDE3iAsUUItT4nhFX6rg3LEKb6fw+hlxKiEiiAC4VoYxhClu+lNOnQvGsRQrbvaZo8QRFsCdPS+//+GzpRE5GlAplJfwkwAMTs80PYBZ3FAAAAAElFTkSuQmCC";
+var css = ".hentry .actions-hover span.reply-to-all-icon {background-image:url(data:image/png;base64," + icon + "); cursor:pointer; height:15px; margin-right:1px; width:15px;} .hentry .actions-hover .reply_to_all:hover .reply-to-all-icon {background-position: -16px 0;}";
+GM_addStyle(css);
 
 
 function getTweeps(status, sep) {
